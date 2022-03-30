a8c590d33112d1b25915dfa9eaffc389d5090bad
Enrique <enrique_alvarado95@hotmail.com>
2022-03-29 21:42:29 -0500

    Layout build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import { AppProps } from "next/app";
-// import Head from "next/head";
+import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
 // import { AuthProvider } from "src/auth/useAuth";
-// import "../styles/index.css";
+//importar estilos de tailwind que vienen de la carpeta de styles/index.css
+import "../styles/index.css";
+
+//aqui en app se van poniendo todos los providers porque engloban todas las paginas de la app
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Home sweet Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
+
+//la configuracion de babel , que es un compilador , es para ser compatible con typescript y graphql
+//el postcss.config.js file es para que se purgen todas las clases de tailwind sin usar
+//en el tsconfig.json hay una opcion muy interesante que dice baseUrl: ".", esto sirve para poder  hacer los imports desde el folder /src/componentes en vez de /../../src/
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,49 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+//las interfaces se usan en react para declarar los tipos de propiedades
+
+//reactnode es el tipado para bloques de react como el string o numbre
+//por ejemplo el tipado para layuoyt es un componente de funcion
+interface IProps {
+  main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+  const authenticated = false;
+  const logout = () => null;
+  return (
+    <div className="bg-gray-900  max-w-screen-2xl mx-auto text-white">
+      <nav className="bg-gray-800 " style={{ height: "64px" }}>
+        <div className="px-6 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img
+                src="/home-color.svg"
+                alt="home logo"
+                className="inline w-6"
+              />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={logout}>Logout</button>
+            </>
+          ) : (
+            <>
+              <Link href="/auth">
+                <a>Login/signup</a>
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
+      <main style={{ minHeight: "calc(100vh - 64px)" }}>{main}</main>
+    </div>
+  );
+};
+export default Layout;
